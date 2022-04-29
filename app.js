f957975ada7a067f5abeb8f846e36ab8a5ba8096
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2022-04-29 23:21:03 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// const colors = ['green','red','rgba(133,122,200)', '#f15025'];
+// const btn = document.getElementById('btn');
+
+// const color = document.querySelector('.color');
+
+// btn.addEventListener('click',function(){
+//    //getrandom number between 0-3
+//     const randomNumber = getRandomNumber();
+//     document.body.style.backgroundColor = colors[randomNumber];
+//     color.textContent = colors[randomNumber]
+// })
+
+// function getRandomNumber(){
+//     return Math.floor(Math.random() * colors.length);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// My work
+
+
+const colors = ['red','blue','green','yellow','rgba(234,54,87)','purple','rgba(12,23,64)','grey'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click',function(){
+
+    let randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+}
